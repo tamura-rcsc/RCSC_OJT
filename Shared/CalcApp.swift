886c98cc5de4a5c6f9e1194080826e7b9762b4df
tamura-rcsc <tamura@rcsc.co.jp>
2021-07-07 11:39:50 +0900

    Initial Commit

---
+++ Shared/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Shared
+//
+//  Created by Aoi Tamura on 2021/07/07.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
